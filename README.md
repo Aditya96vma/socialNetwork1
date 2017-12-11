3d50252630b0b0222fd60271ee5a71d0bbb48c42
Adi_Vic <aditya96vma@gmail.com>
2017-12-11 20:43:00 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,2 @@
+This Project was made by Aditya with the help of online sources .
+This is my first venture into coding and creating apps.
---
+++ src/screens/index.js
@@ -0,0 +1,10 @@
+
+
+export * from "./Home";
+export * from "./Login";
+export * from "./TabScreen";
+
+export * from "./Profile";
+
+export * from "./Tweet";
+
